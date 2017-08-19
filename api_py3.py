4560fc0f01f6a88ed98101d7e25f651dc1bf7259
不插电 <se4@live.cn>
2017-08-19 21:45:00 +0800

    增加api文件

---
+++ api_py3.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+
+"""
+成功请求的参数顺序：
+signature,nonce,key
+nonce,singature,key
+(nonce参数一定要在key前头)
+"""
+
+import requests
+import time
+import hashlib
+import hmac
+import collections
+
+
+class Jubi(object):
+
+    def __init__(self, public_key, private_key):
+        self.public_key = public_key
+        self.private_key = private_key
+
+    def get_nonce(self):
+        lens = 12
+        curr_stamp = time.time()*100
+        nonce = int(curr_stamp)
+        return nonce
+
+    def get_hash(self,s):
+        m = hashlib.md5()
+        m.update(s.encode())
+        return m.hexdigest()
+
+    def get_params(self):
+        nonce_value = self.get_nonce()
+        key_value = self.public_key
+        private_key = self.private_key
+        msg = 'nonce=' + str(nonce_value) + '&' + 'key=' + key_value
+        msg = msg.encode('utf-8')
+        key = self.get_hash(private_key)
+        key = key.encode('utf-8')
+        signature = hmac.new(key, msg, digestmod=hashlib.sha256).hexdigest()
+        dict_ordered = collections.OrderedDict()
+        dict_ordered['signature'] = signature
+        dict_ordered['nonce'] = nonce_value
+        dict_ordered['key'] = key_value
+        #output_dict = {'nonce': nonce_value, 'key': key_value, 'signature': signature}
+        return dict_ordered
+
+
+
+#jubi = Jubi('your_public_key','your_private_key')
+#data = jubi.get_params()
+#print(data)
+#resp = requests.post(url='https://www.jubi.com/api/v1/balance/',data=data).text
+#print(resp)
